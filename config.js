da87f852ae2acad69a65c21bddc7f23fbd2aedc1
savinda12345 <sesandasudesh@gmail.com>
2025-02-26 19:46:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || '', //Put Session-id Here
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || '', //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '', //Put Your Github Auth Token
+SESSION_ID: process.env.SESSION_ID || 'SEON-MD=RYA0nTzZ#KkK-M4xzTVZRVhmugFxng0rOStCVsoTtWawKmVZh8BA', //Put Session-id Here
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || 'savinda12345', //Put Your Gitub Username
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || 'vU62L4AQFamwKm9UDN8oJ4rAlyME7D1a6go8', //Put Your Github Auth Token
 DATABASE_REPO_NAME: process.env.DATABASE_REPO_NAME || 'SEON-MD-DATABASE' //Bot Database Repo Name
 };
